4d914e2a9c5fcfed061971eac6c29034fb162407
othaderek <otha.hernandez@gmail.com>
2019-06-05 17:38:26 -0400

    We did it

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-Artist.destroy_all
+# Artist.destroy_all
 
 10.times do
   Artist.create(name: Faker::Name.name, age: rand(1..100), bio: Faker::Hipster.sentence)
 end
 
 puts "File has been seeded! 🍀"
+
+
+# Artist.new("Tony", 70, "i'm getting older")
+# Artist.new("Kevin", 45, "im no that old")
+# Artist.new("Greg", 7, "i'm young")
