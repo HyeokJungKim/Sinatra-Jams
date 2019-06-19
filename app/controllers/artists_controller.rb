0f5eae8e690df67a0a516e43211ba9f3599325b0
maxsunnyday <max.sun@yale.edu>
2019-06-19 14:39:20 -0400

    done

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -22,12 +22,21 @@ class ArtistsController < ApplicationController
       #edit shit
       get "/artists/:id/edit" do
         @artist = Artist.find_by_id(params[:id])
+        @instruments = Instrument.all
         erb :edit
       end
     
       patch "/artists/:id" do
         artist = Artist.find_by_id(params[:id])
         artist.update(name: params[:name], age: params[:age], bio: params[:bio])
+
+        instruments = Instrument.all.select do |instrument|
+            if params[:instrument]
+                params[:instrument][instrument.id.to_s]
+            end
+        end
+        artist.instruments = instruments
+        artist.save
         redirect to("/artists/#{artist.id}")
       end
     
