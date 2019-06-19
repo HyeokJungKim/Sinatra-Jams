fa3f87f8c7e70cecf0a0589d679e53489436183f
maxsunnyday <max.sun@yale.edu>
2019-06-19 13:43:35 -0400

    halfway there

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,42 +10,4 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :welcome
   end
-
-  get "/artists" do
-    @artists = Artist.all
-    erb :index
-  end
-
-  get "/artists/new" do
-    erb :new
-  end
-
-  get "/artists/:id" do
-    @artist = Artist.find(params[:id])
-
-    erb :show
-  end
-
-  post "/artists" do
-    @artist = Artist.create(params)
-    redirect to ("/artists/#{@artist.id}")
-  end
-
-  #edit shit
-  get "/artists/:id/edit" do
-    @artist = Artist.find_by_id(params[:id])
-    erb :edit
-  end
-
-  patch "/artists/:id" do
-    artist = Artist.find_by_id(params[:id])
-    artist.update(name: params[:name], age: params[:age], bio: params[:bio])
-    redirect to("/artists/#{artist.id}")
-  end
-
-  delete "/artists/:id" do
-    Artist.destroy(params[:id])
-
-    redirect to("/artists")
-  end
 end
