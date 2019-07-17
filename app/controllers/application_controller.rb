d56ea468d6e61eb3519ec7d6cd66f54cbf4114e7
Ishmet Jahan <ishmetjahan@yahoo.com>
2019-07-17 17:38:46 -0400

    pair with Daniel V.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,49 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
 
   get "/" do
     erb :welcome
   end
+
+  get "/artists" do 
+    @artists= Artist.all
+    erb :index
+  end
+
+  get "/artists/new" do 
+    erb :new
+  end
+ 
+  get "/artists/:id" do 
+    #binding.pry
+    @artist= Artist.find(params[:id])
+    erb :show
+  end
+
+  post "/artists" do
+    @artist= Artist.create(params[:artist])
+    redirect to "/artists/#{@artist.id}"
+  end
+
+  get "/artists/:id/edit" do 
+    @artist= Artist.find(params[:id])
+    erb :edit
+  end
+
+  put "/artists/:id" do 
+    @artist= Artist.find(params[:id])
+    @artist.update(params[:artist])
+    redirect to "/artists/#{@artist.id}"
+  end
+
+  delete "/artists/:id" do 
+    @artist= Artist.find(params[:id])
+    @artist.destroy
+    redirect to "/artists"
+  end
+  
 
 end
