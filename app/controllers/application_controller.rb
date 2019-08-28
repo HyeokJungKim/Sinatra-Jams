03c5e7f949b617a3cd5dc504b9e1c5d6ac019b51
Brian Wong <bwong92@gmail.com>
2019-08-28 16:51:48 -0400

    CR done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
@@ -10,5 +12,25 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :welcome
   end
+
+  get "/artist" do   
+    erb :artist
+  end
+  #CREATE
+  get "/artist/new" do
+    erb :new
+  end
+  
+  #READ
+  get "/artist/:id" do 
+    @artist = Artist.find(params[:id])
+    erb :show
+  end
+
+  post "/artist" do   
+    Artist.create(params[:artist])
+    redirect "/artist"
+  end
+
 
 end
